27bb4411ea471fc0552dd6a56d68364ed3ee2a6e
saci zakaria <sacideveloppeur@gmail.com>
2023-01-13 17:00:18 +0100

    chaînes de caractères

---
+++ testPython.py
@@ -0,0 +1,9 @@
+print("hello")
+print("C:\Git\mingw64")
+
+###########################################
+# 64. Les chaînes de caractères
+site = 'docstring'
+
+print(site[0])  # d
+print(site[4])  # t
