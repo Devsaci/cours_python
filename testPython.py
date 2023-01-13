ec09b77bfd1cf02a96f4b6ac9a4d2acd162b3263
saci zakaria <sacideveloppeur@gmail.com>
2023-01-13 17:08:19 +0100

    longue_phrase  et guillemets doubles

--- testPython.py
+++ testPython.py
@@ -7,3 +7,15 @@ site = 'docstring'
 
 print(site[0])  # d
 print(site[4])  # t
+
+
+# longue_phrase  et guillemets doubles
+
+age = "J'ai 26 ans"  # Avec guillemets doubles
+age = 'J\'ai 26 ans'  # Avec guillemets simples
+longue_phrase = '''Bonjour,
+bienvenue sur Docstring, 
+on espère que vous appréciez 
+ce glossaire sur les chaînes
+de caractères 🙂
+'''
