80b6c436291546f36796ccac2f18a4e629290842
saci zakaria <sacideveloppeur@gmail.com>
2023-01-19 21:17:16 +0100

    Comment

--- calcukatrive.py
+++ calcukatrive.py
@@ -1,7 +1,10 @@
 # Caculatrice fonction
 a = input("Entrez un nombre a :  ")
-print(a)
+print(a)  # retourn chaine de caractere
 b = input("Entrez un nombre b :  ")
 print(b)
 print(
     f"Le résultat de l'addition de {a} avec {b} est égal à {int(a) + int(b)}")
+
+# {} affiche la variable
+# int() transforme str -> int
