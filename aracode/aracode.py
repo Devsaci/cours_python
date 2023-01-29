0f6ed0f1cf7a5e492171fd237dc7a6e4614b4698
saci zakaria <sacideveloppeur@gmail.com>
2023-01-29 15:37:15 +0100

    print(var.isidentifier())

--- aracode/aracode.py
+++ aracode/aracode.py
@@ -1,2 +1,8 @@
 # راكوان للبرمجة - CodeRK
 # https://www.youtube.com/watch?v=m_wEgw42g0E&list=PLSiLeKadTQ7mT2nyzPbfzWP3IOniezNvP&index=1
+
+var = 'tesla'
+print(var.isidentifier())
+# True
+print(var.isidentifier)
+# <built-in method isidentifier of str object at 0x00000223A5763F30>
