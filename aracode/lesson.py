3abac443618d916ba12e64d6b096ce6a11a5e9e2
saci zakaria <sacideveloppeur@gmail.com>
2023-02-28 09:19:52 +0100

    python -m tkinter

--- aracode/lesson.py
+++ aracode/lesson.py
@@ -1,4 +1,10 @@
-
+# -----------------------Start---------------------------#
 # تعلم لغة البرمجة بايثون :
 # تعلم الاساسيات وتحويلها بشكل مباشر لواجهة وتحويلها لتطبيق تنفيذي exe
 # https://www.youtube.com/watch?v=AtgN44i4WZE&t=215s
+
+# https://docs.python.org/fr/3/library/tkinter.html#a-hello-world-program
+
+from tkinter import *
+pro = Tk()
+pro.mainloop()
