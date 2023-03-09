44836cba7778ca880d74247d1c20b35f66fb39c2
saci zakaria <sacideveloppeur@gmail.com>
2023-03-09 09:08:14 +0100

    if not

--- houdon_T/calculatrice3.py
+++ houdon_T/calculatrice3.py
@@ -6,3 +6,7 @@ while not (a.isdigit() and b.isdigit()):
     # On demande deux nombres à l'utilisateur
     a = input("Entrez un premier nombre : ")
     b = input("Entrez un deuxième nombre : ")
+
+    # On affiche une phrase si les nombres entrés ne sont pas valides.
+    if not (a.isdigit() and b.isdigit()):
+        print("Veuillez entrer deux nombres valides")
